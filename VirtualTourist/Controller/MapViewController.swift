5554d809c2f05e9d5ba4f3ddd4de7d682e31fdb8
user140243 <user140243@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-06-04 00:08:23 -0400

    Storyboard modifications in preparation for collection view integration. work on constraints needed

--- VirtualTourist/Controller/MapViewController.swift
+++ VirtualTourist/Controller/MapViewController.swift
@@ -12,10 +12,27 @@ import CoreData
 
 class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate{
     //var dataController: DataController!
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var navigationBar: UINavigationItem!
+    @IBOutlet weak var editPinsButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //View Defaults
+        navigationBar.title = "Virtual Tourist"
     }
+    
     var dataController:
     DataController!
     
+    //MARK: General Configurations
+    func configureMap() {
+        mapView.delegate = self
+        let location = CLLocationCoordinate2DMake(35, 139)
+        let mapSpan = MKCoordinateSpanMake(25, 25)
+        let region = MKCoordinateRegionMake(location,mapSpan)
+        self.mapView.setRegion(region, animated: true)
+    }
 }
