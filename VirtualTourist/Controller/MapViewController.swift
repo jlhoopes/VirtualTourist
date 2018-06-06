2227a8662b837c9c0ae3da8cb90fb0d2236514f1
user140279 <user140279@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-06-06 18:58:02 -0400

    adding geolocation

--- VirtualTourist/Controller/MapViewController.swift
+++ VirtualTourist/Controller/MapViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import MapKit
 import CoreData
+import CoreLocation
 
 class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate{
     //var dataController: DataController!
@@ -17,32 +18,50 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
     @IBOutlet weak var navigationBar: UINavigationItem!
     @IBOutlet weak var editPinsButton: UIBarButtonItem!
     
+    var locManager = CLLocationManager()
+    var locationManager = CLLocationManager()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //View Defaults
         navigationBar.title = "Virtual Tourist"
+        configureMap()
+                //print("jinkies")
     }
-    
     
     var dataController:
     DataController!
     
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let lat = locations.last?.coordinate.latitude, let long = locations.last?.coordinate.longitude {
+            print("\(lat),\(long)")
+        } else {
+            print("No coordinates")
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+    
     //MARK: General Configurations
     func configureMap() {
-        
-        let locManager = CLLocationManager()
-        locManager.requestWhenInUseAuthorization()
-        var currentLocation: CLLocation!
-        if( CLLocationManager.authorizationStatus() == .authorizedWhenInUse ||
-            CLLocationManager.authorizationStatus() ==  .authorizedAlways){
-            currentLocation = locManager.location
-        }
-        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+        var lat = locationManager.location?.coordinate.latitude
+        var lon = locationManager.location?.coordinate.longitude
+        //print("this")
+        //print(locationManager.location?.coordinate.latitude)
+
         mapView.delegate = self
-        let location = CLLocationCoordinate2DMake(currentLocation.coordinate.longitude, currentLocation.coordinate.latitude)
+        
+        //let location = CLLocationCoordinate2DMake(lon!, lat!)
+        let location = CLLocationCoordinate2DMake(25, 35)
         let mapSpan = MKCoordinateSpanMake(25, 25)
         let region = MKCoordinateRegionMake(location,mapSpan)
         self.mapView.setRegion(region, animated: true)
+        
     }
 }
